92f5e764e28a6591b40d25c64b715c4fee35238b
Geodrop Studios <geodropstudios@gmail.com>
2020-07-12 05:30:57 +0200

    animate background

---
+++ Maldimensional/Assets/Scripts/CloudAnimator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudAnimator : MonoBehaviour {
+
+#pragma warning disable
+    [SerializeField]
+    private GameObject initialLeftClouds;
+    [SerializeField]
+    private GameObject initialCenterClouds;
+    [SerializeField]
+    private GameObject initialRightClouds;
+    [SerializeField]
+    private float deltaMovement;
+#pragma warning restore
+
+    private GameObject leftClouds;
+    private GameObject centerClouds;
+    private GameObject rightClouds;
+
+    void Start() {
+        leftClouds = initialLeftClouds;
+        centerClouds = initialCenterClouds;
+        rightClouds = initialRightClouds;
+    }
+
+    void Update() {
+        leftClouds.transform.position += Vector3.left * deltaMovement * Time.deltaTime;
+        centerClouds.transform.position += Vector3.left * deltaMovement * Time.deltaTime;
+        rightClouds.transform.position += Vector3.left * deltaMovement * Time.deltaTime;
+
+        if (centerClouds.transform.position.x <= 0.0f) {
+            leftClouds.transform.position = rightClouds.transform.position + Vector3.right * 20.0f;
+
+            GameObject tempOldLeftClouds = leftClouds;
+            leftClouds = centerClouds;
+            centerClouds = rightClouds;
+            rightClouds = tempOldLeftClouds;
+        }
+    }
+}
